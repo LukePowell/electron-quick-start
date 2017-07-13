842f1408efb32fe1b0e53731231355091c4721c2
Luke Powell <raistlinpowell@gmail.com>
2017-07-13 15:37:45 -0700

    Using showMessageBox

--- renderer.js
+++ renderer.js
@@ -2,7 +2,15 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 const {dialog} = require('electron').remote
+const {BrowserWindow} = require('electron').remote
 
 exports.error = function(){
   dialog.showErrorBox('This should be modal', 'Works on macOS but not Windows.');
+}
+
+exports.errorShowMessageBox = function(){
+  dialog.showMessageBox(BrowserWindow.getFocusedWindow(), {
+    type: 'error',
+    message: 'This should also be modal'
+  });
 }
